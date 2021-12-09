b4e519cc2a0d5b602305bdb3f82a6b09d755ce8c
Makkusu-Tieu <23tieuk@live.elida.k12.oh.us>
2021-12-09 10:01:27 -0500

    Player Movement
    Completed Player Movement Script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     public float speed;
     private float yRange = 4;
 
+    public GameObject projectilePrefab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +19,22 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation); 
+        }
+
+        if (transform.position.y > yRange)
+        {
+            transform.position = new Vector2(transform.position.x, yRange);
+        }
+
+        if (transform.position.y < -yRange)
+        {
+            transform.position = new Vector2(transform.position.x, -yRange);
+        }
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector2.up * verticalInput * Time.deltaTime * speed);
     }
 }
