c929b3841162bb5d7c2c8ccc243ca575ee7ec334
Makkusu-Tieu <23tieuk@live.elida.k12.oh.us>
2021-12-13 10:03:36 -0500

    In Progress Spawn Manager
    Working on Randomly Spawning Aliens

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] alienPrefabs;
+    private float spawnRangeX = 11.0f;
+    int[] spawnRangeY = {0, 2, 4};
 
     // Start is called before the first frame update
     void Start()
@@ -16,5 +18,12 @@ public class SpawnManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void SpawnRandomAlien()
+    {
+        //Vector2 spawnPos = new Vector2(spawnRangeX, spawnRangeY);
+        int alienIndex = (Random.Range(0, alienPrefabs.Length));
+        //Instatiate(alienPrefabs[alienIndex], spawnPos, alienPrefabs[animalIndex].transform.rotation);
     }
 }
