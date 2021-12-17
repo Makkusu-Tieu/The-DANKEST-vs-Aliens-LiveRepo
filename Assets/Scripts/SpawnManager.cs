2be73d6dbc27ed03a718df3beb364b6a21df1441
Makkusu-Tieu <23tieuk@live.elida.k12.oh.us>
2021-12-17 09:18:18 -0500

    WIP Spawn Manager
    Tried using an array

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -8,6 +8,7 @@ public class SpawnManager : MonoBehaviour
     private float spawnRangeX = 11.0f;
     private float startDelay = 1.0f;
     private float spawnInterval = 1.0f;
+    //private int[] spawnRangeY = { -4, -2, 0, 2, 4 };
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +25,7 @@ public class SpawnManager : MonoBehaviour
     void SpawnRandomAlien()
     {
         Vector2 spawnPos = new Vector2(spawnRangeX, Random.Range(-4, 4));
+        //Vector2 spawnPos = new Vector2(spawnRangeX, spawnRangeY(0, 4));
         int alienIndex = (Random.Range(0, alienPrefabs.Length));
         Instantiate(alienPrefabs[alienIndex], spawnPos, alienPrefabs[alienIndex].transform.rotation);
     }
