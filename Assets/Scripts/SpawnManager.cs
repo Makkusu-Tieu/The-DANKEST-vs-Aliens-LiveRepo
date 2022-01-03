fd0ac81c2377da5c2971e2463193f3de784cf274
Makkusu-Tieu <23tieuk@live.elida.k12.oh.us>
2022-01-03 10:03:11 -0500

    Added Alien OutOfBounds

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -9,6 +9,13 @@ public class SpawnManager : MonoBehaviour
     private float startDelay = 1.0f;
     private float spawnInterval = 1.0f;
     //private int[] spawnRangeY = { -4, -2, 0, 2, 4 };
+    //private int yRange = spawnRangeY[Random.Range(0, 4)];
+
+    //private int laneOne = 4;
+    //private int laneTwo = 2;
+    //private int laneThree = 0;
+    //private int laneFour = -2;
+    //private int laneFive = -4;
 
     // Start is called before the first frame update
     void Start()
@@ -25,7 +32,10 @@ public class SpawnManager : MonoBehaviour
     void SpawnRandomAlien()
     {
         Vector2 spawnPos = new Vector2(spawnRangeX, Random.Range(-4, 4));
-        //Vector2 spawnPos = new Vector2(spawnRangeX, spawnRangeY(0, 4));
+        //Vector2 spawnPos = new Vector2(spawnRangeX, Random.Range(spawnRangeY));
+        //Vector2 spawnPos = new Vector2(spawnRangeX, Random.Next(0, spawnRangeY.Length));
+        //Vector2 spawnPos = new Vector2(spawnRangeX, Random.Next(laneOne, laneTwo, laneThree, laneFour, laneFive));
+        //Vector2 spawnPos = new Vector2(spawnRangeX, yRange);
         int alienIndex = (Random.Range(0, alienPrefabs.Length));
         Instantiate(alienPrefabs[alienIndex], spawnPos, alienPrefabs[alienIndex].transform.rotation);
     }
