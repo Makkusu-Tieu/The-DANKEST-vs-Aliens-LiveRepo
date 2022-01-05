1f76603cb9824aefbe83ce8fc0704c1b875db400
Makkusu-Tieu <23tieuk@live.elida.k12.oh.us>
2022-01-05 10:01:03 -0500

    Jan 5

--- Assets/Scripts/DestroyOutOfBounds2.cs
+++ Assets/Scripts/DestroyOutOfBounds2.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DestroyOutOfBounds2 : MonoBehaviour
 {
-    private float leftbound = -10f;
+    private float leftbound = -12f;
 
     // Start is called before the first frame update
     void Start()
