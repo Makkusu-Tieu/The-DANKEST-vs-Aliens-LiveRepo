edfd3c352311bfc21c5aafa2906d76de5ab11a80
Makkusu-Tieu <23tieuk@live.elida.k12.oh.us>
2022-01-05 09:36:50 -0500

    Spawn Manager (Completed)
    Aliens now spawn at Y: -4, -2, 0, 2, 4

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -9,13 +9,7 @@ public class SpawnManager : MonoBehaviour
     private float startDelay = 1.0f;
     private float spawnInterval = 1.0f;
     private int[] spawnRangeY = { -4, -2, 0, 2, 4 };
-    //private int yRange = spawnRangeY[Random.Range(0, 4)];
-
-    //private int laneOne = 4;
-    //private int laneTwo = 2;
-    //private int laneThree = 0;
-    //private int laneFour = -2;
-    //private int laneFive = -4;
+    private int alienSpawnPosY;
 
     // Start is called before the first frame update
     void Start()
@@ -31,12 +25,10 @@ public class SpawnManager : MonoBehaviour
 
     void SpawnRandomAlien()
     {
-        //Vector2 spawnPos = new Vector2(spawnRangeX, Random.Range(-4, 4));
-        Vector2 spawnPos = new Vector2(spawnRangeX, Random.Range(-4, spawnRangeY.Length));
+        alienSpawnPosY = spawnRangeY[Random.Range(0, 5)];
+        Vector2 spawnPos = new Vector2(spawnRangeX, alienSpawnPosY);
         Debug.Log("spawnPos" + spawnPos);
-        //Vector2 spawnPos = new Vector2(spawnRangeX, Random.Next(0, spawnRangeY.Length));
-        //Vector2 spawnPos = new Vector2(spawnRangeX, Random.Next(laneOne, laneTwo, laneThree, laneFour, laneFive));
-        //Vector2 spawnPos = new Vector2(spawnRangeX, yRange);
+        
         int alienIndex = (Random.Range(0, alienPrefabs.Length));
         Instantiate(alienPrefabs[alienIndex], spawnPos, alienPrefabs[alienIndex].transform.rotation);
     }
