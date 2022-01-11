48c4d3313cada3a85f36e1541fbea075526518e7
Makkusu-Tieu <23tieuk@live.elida.k12.oh.us>
2022-01-11 10:02:04 -0500

    HP Bar (WIP)

--- Assets/Scripts/WallController.cs
+++ Assets/Scripts/WallController.cs
@@ -7,10 +7,13 @@ public class WallController : MonoBehaviour
     public int maxHealth;
     public int currentHealth;
 
+    public HealthBar healthBar;
+
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
 
     // Update is called once per frame
@@ -31,7 +34,8 @@ public class WallController : MonoBehaviour
     void TakeDamage(int damage)
     {
         currentHealth -= damage;
-        Debug.Log("Current Health:" + currentHealth);
+
+        healthBar.SetMaxHealth(maxHealth);
 
         if (currentHealth < 1)
         {
