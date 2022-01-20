df27cc8fd1c18d5f91f4b3c7743f65c0e1ec1eb7
Makkusu-Tieu <23tieuk@live.elida.k12.oh.us>
2022-01-20 10:03:47 -0500

    Wave Text (WIP), Wave Spawn Script Completed

---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public float spawnRate = 1.0f;
+    public float timeBetweenWaves = 3.0f;
+
+    public int enemyCount;
+
+    public GameObject alien;
+
+    bool waveIsDone = true;
+
+    private float spawnRangeX = 11.0f;
+    private int[] spawnRangeY = { -4, -2, 0, 2, 4 };
+    private int alienSpawnPosY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (waveIsDone == true)
+        {
+            StartCoroutine(waveSpawner());
+        }
+    }
+
+    IEnumerator waveSpawner()
+    {
+        waveIsDone = false;
+
+        for (int i = 0; i < enemyCount; i++)
+        {
+            //GameObject enemyClone = Instantiate(alien);
+            alienSpawnPosY = spawnRangeY[Random.Range(0, 5)];
+            Vector2 spawnPos = new Vector2(spawnRangeX, alienSpawnPosY);
+            Instantiate(alien, spawnPos, alien.transform.rotation);
+
+            yield return new WaitForSeconds(spawnRate);
+        }
+
+        spawnRate -= 0.1f;
+        enemyCount += 3;
+
+        yield return new WaitForSeconds(timeBetweenWaves);
+
+        waveIsDone = true;
+    }
+}
